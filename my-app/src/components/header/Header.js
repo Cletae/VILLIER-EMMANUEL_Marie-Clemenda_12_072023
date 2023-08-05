10bb52e23fa80989f70c811775cd2bbfc9e3b37e
Souvi <souvima@hotmail.com>
2023-08-06 00:43:29 +0200

    Add header

---
+++ my-app/src/components/header/Header.js
@@ -0,0 +1,40 @@
+import { Link } from "react-router-dom";
+import "./Header.css";
+import logo from "../../images/logo.png";
+
+function Header() {
+  return (
+    <header>
+      <nav className="navbar">
+        <Link to="/" className="navbar-logo">
+          <img src={logo} alt="Logo-image" className="logo" />
+        </Link>
+
+        <ul className="nav-menu">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              Accueil
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/about" className="nav-link">
+              Profil
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              Réglage
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/about" className="nav-link">
+              Communauté
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
